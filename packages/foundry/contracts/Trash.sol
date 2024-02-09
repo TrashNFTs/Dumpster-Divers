95d0d89b591d90eb8c5cdc3719f2369e2e031021
Jacob Homanics <homanicsjake@gmail.com>
2024-02-09 14:35:43 -0600

    added in complete project

---
+++ packages/foundry/contracts/Trash.sol
@@ -0,0 +1,83 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "./ERC404.sol";
+import "./Strings.sol";
+
+contract Trash is ERC404 {
+    string public dataURI;
+    string public baseTokenURI;
+
+    constructor(
+        address _owner
+    ) ERC404("Dumpster Divers", "TRASH", 18, 10000, _owner) {
+        balanceOf[_owner] = 10000 * 10 ** 18;
+    }
+
+    function setDataURI(string memory _dataURI) public onlyOwner {
+        dataURI = _dataURI;
+    }
+
+    function setTokenURI(string memory _tokenURI) public onlyOwner {
+        baseTokenURI = _tokenURI;
+    }
+
+    function setNameSymbol(
+        string memory _name,
+        string memory _symbol
+    ) public onlyOwner {
+        _setNameSymbol(_name, _symbol);
+    }
+
+    function tokenURI(uint256 id) public view override returns (string memory) {
+        if (bytes(baseTokenURI).length > 0) {
+            return string.concat(baseTokenURI, Strings.toString(id));
+        } else {
+            uint8 seed = uint8(bytes1(keccak256(abi.encodePacked(id))));
+            string memory image;
+            string memory color;
+
+            if (seed <= 100) {
+                image = "GREY.PNG";
+                color = "Grey";
+            } else if (seed <= 160) {
+                image = "BLUE.PNG";
+                color = "Blue";
+            } else if (seed <= 210) {
+                image = "GREEN.PNG";
+                color = "Green";
+            } else if (seed <= 240) {
+                image = "YELLOW.PNG";
+                color = "Yellow";
+            } else if (seed <= 255) {
+                image = "PINK.PNG";
+                color = "Pink";
+            }
+
+            string memory jsonPreImage = string.concat(
+                string.concat(
+                    string.concat(
+                        '{"name": "Dumpster Divers #',
+                        Strings.toString(id)
+                    ),
+                    '","description":"A collection of 10,000 trashy Dumpster Divers enabled by ERC404, an experimental token standard pioneered by Pandora.","external_url":"https://trashnfts.com","image":"'
+                ),
+                string.concat(dataURI, image)
+            );
+            string memory jsonPostImage = string.concat(
+                '","attributes":[{"trait_type":"Color","value":"',
+                color
+            );
+            string memory jsonPostTraits = '"}]}';
+
+            return
+                string.concat(
+                    "data:application/json;utf8,",
+                    string.concat(
+                        string.concat(jsonPreImage, jsonPostImage),
+                        jsonPostTraits
+                    )
+                );
+        }
+    }
+}
---
+++ packages/foundry/script/Deploy.s.sol
@@ -0,0 +1,54 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// import "../contracts/YourContract.sol";
+import "./DeployHelpers.s.sol";
+import "../contracts/Trash.sol";
+
+contract DeployScript is ScaffoldETHDeploy {
+    error InvalidPrivateKey(string);
+
+    address owner = 0xBd1413d4C670c0e1957C3338Eadd6535CB1c562d; // starts as deployer, set to treasury once deployed
+
+    function run() external {
+        uint256 deployerPrivateKey = setupLocalhostEnv();
+        if (deployerPrivateKey == 0) {
+            revert InvalidPrivateKey(
+                "You don't have a deployer account. Make sure you have set DEPLOYER_PRIVATE_KEY in .env or use `yarn generate` to generate a new random account"
+            );
+        }
+        address deployerPubKey = vm.createWallet(deployerPrivateKey).addr;
+
+        owner = deployerPubKey;
+
+        vm.startBroadcast(deployerPrivateKey);
+        Trash dd = new Trash(deployerPubKey);
+
+        dd.setDataURI(
+            "ipfs://bafybeiclqcx3kdoauwelxgcny25wauci6qqonfigid6y2wrv4ep4gji3gq/"
+        );
+
+        // dd.setWhitelist(deployerPubKey, true);
+        dd.setWhitelist(owner, true);
+        dd.transfer(owner, 10000 * 10 ** 18);
+
+        dd.transferOwnership(owner);
+
+        console.logString(
+            string.concat(
+                "YourContract deployed at: ",
+                vm.toString(address(dd))
+            )
+        );
+        vm.stopBroadcast();
+
+        /**
+         * This function generates the file containing the contracts Abi definitions.
+         * These definitions are used to derive the types needed in the custom scaffold-eth hooks, for example.
+         * This function should be called last.
+         */
+        exportDeployments();
+    }
+
+    function test() public {}
+}
