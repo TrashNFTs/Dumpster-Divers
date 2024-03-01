a67448c7ae8d8be8267dd58993de4dbbcc9b7004
Jacob Homanics <homanicsjake@gmail.com>
2024-03-01 00:37:41 -0700

    updated swap

--- packages/foundry/test/YourContract.t.sol
+++ packages/foundry/test/YourContract.t.sol
@@ -31,71 +31,154 @@ contract YourContractTest is Test {
         vm.stopPrank();
     }
 
-    function testMintBatch(uint256 numOfNfts) public {
-        uint256 maxNfts = 10000;
-        vm.assume(numOfNfts > 0);
-        vm.assume(numOfNfts <= maxNfts);
+    function generateRandomNumber(
+        uint256 rarityOffset,
+        uint256 seed
+    ) public view returns (uint256) {
+        // uint256 offset = 0;
+        // for (uint256 i = 0; i <= rarityOffset; i++) {
+        //     offset += 20000;
+        // }
 
-        console.log(numOfNfts);
-        vm.prank(deployer);
-        trash.setWhitelist(deployer, true);
+        uint256 blockNumber = block.number - rarityOffset; // Use the previous block's hash
+        bytes32 blockHash = blockhash(blockNumber);
+        return (uint256(blockHash) % seed) + (rarityOffset * 20000);
+    }
 
-        assertEq(trash.balanceOf(deployer), maxNfts * 10 ** 18);
+    function testMe(uint256 bn) external {
+        vm.assume(bn > 100000);
 
-        string[] memory originalOwnerTokenURIs = new string[](numOfNfts);
+        vm.warp(1641070800);
+        vm.roll(bn);
 
-        for (uint256 i = 1; i <= numOfNfts; i++) {
-            originalOwnerTokenURIs[i - 1] = trash.tokenURI(i);
-        }
-
-        vm.prank(deployer);
-        trash.transfer(user, numOfNfts * 10 ** 18);
-
-        assertEq(trash.balanceOf(deployer), (maxNfts - numOfNfts) * 10 ** 18);
-
-        string[] memory originalTokenURIs = new string[](numOfNfts);
-
-        for (uint256 i = 1; i <= numOfNfts; i++) {
-            assertEq(trash.ownerOf(i), user);
-            originalTokenURIs[i - 1] = trash.tokenURI(i);
-            assertEq(originalTokenURIs[i - 1], originalOwnerTokenURIs[i - 1]);
-        }
-
-        vm.startPrank(user);
-        trash.setApprovalForAll(address(dumpsterBin), true);
-
-        uint256[] memory ids = new uint256[](numOfNfts);
-        for (uint256 i = 0; i < numOfNfts; i++) {
-            ids[i] = i + 1;
-        }
-
-        dumpsterBin.mintBatch(ids);
-
-        for (uint256 i = 0; i < numOfNfts; i++) {
-            assertEq(trash.ownerOf(i + 1), address(dumpsterBin));
-            assertEq(dumpsterDivers.ownerOf(i), user);
-        }
-
-        uint256[] memory ids2 = new uint256[](numOfNfts);
-        for (uint256 i = 0; i < numOfNfts; i++) {
-            ids2[i] = i;
-        }
-
-        dumpsterBin.burnBatch(ids2);
-
-        vm.stopPrank();
-
-        for (uint256 i = 1; i <= numOfNfts; i++) {
-            assertEq(trash.ownerOf(i), user);
-            vm.expectRevert();
-            dumpsterDivers.ownerOf(i - 1);
+        for (uint256 i = 0; i < 20000; i++) {
+            uint256 rn = generateRandomNumber(0, 20000);
+            assertEq(rn < 20000, true);
         }
     }
 
-    function testRevert__CannotMintIfNotOwnerOfBin(address addr) public {
-        vm.assume(addr != address(dumpsterBin));
-        vm.prank(user);
-        vm.expectRevert();
-        dumpsterDivers.mint(user, 0);
+    function testMe2(uint256 bn) external {
+        vm.assume(bn > 100000);
+
+        vm.warp(1641070800);
+        vm.roll(bn);
+
+        for (uint256 i = 0; i < 20000; i++) {
+            uint256 rn = generateRandomNumber(1, 20000);
+            assertEq(rn >= 20000 && rn < 40000, true);
+        }
     }
+
+    function testMe3(uint256 bn) external {
+        vm.assume(bn > 100000);
+
+        vm.warp(1641070800);
+        vm.roll(bn);
+
+        for (uint256 i = 0; i < 20000; i++) {
+            uint256 rn = generateRandomNumber(2, 20000);
+            assertEq(rn >= 40000 && rn < 60000, true);
+        }
+    }
+
+    function testMe4(uint256 bn) external {
+        vm.assume(bn > 100000);
+
+        vm.warp(1641070800);
+        vm.roll(bn);
+
+        for (uint256 i = 0; i < 20000; i++) {
+            uint256 rn = generateRandomNumber(3, 20000);
+            assertEq(rn >= 60000 && rn < 80000, true);
+        }
+    }
+
+    function testMe5(uint256 bn) external {
+        vm.assume(bn > 100000);
+
+        vm.warp(1641070800);
+        vm.roll(bn);
+
+        for (uint256 i = 0; i < 20000; i++) {
+            uint256 rn = generateRandomNumber(4, 20000);
+            assertEq(rn >= 80000 && rn < 100000, true);
+        }
+    }
+
+    // function testStrict() external {
+    //     vm.warp(1641070800);
+    //     vm.roll(50000);
+
+    //     uint256 rn = generateRandomNumber(1, 20000);
+    //     console.log(rn);
+    //     // assertEq(rn > 20000 && rn <= 40001, true);
+    // }
+
+    // function testMintBatch(uint256 numOfNfts) public {
+    //     uint256 maxNfts = 10000;
+    //     vm.assume(numOfNfts > 0);
+    //     vm.assume(numOfNfts <= maxNfts);
+
+    //     console.log(numOfNfts);
+    //     vm.prank(deployer);
+    //     trash.setWhitelist(deployer, true);
+
+    //     assertEq(trash.balanceOf(deployer), maxNfts * 10 ** 18);
+
+    //     string[] memory originalOwnerTokenURIs = new string[](numOfNfts);
+
+    //     for (uint256 i = 1; i <= numOfNfts; i++) {
+    //         originalOwnerTokenURIs[i - 1] = trash.tokenURI(i);
+    //     }
+
+    //     vm.prank(deployer);
+    //     trash.transfer(user, numOfNfts * 10 ** 18);
+
+    //     assertEq(trash.balanceOf(deployer), (maxNfts - numOfNfts) * 10 ** 18);
+
+    //     string[] memory originalTokenURIs = new string[](numOfNfts);
+
+    //     for (uint256 i = 1; i <= numOfNfts; i++) {
+    //         assertEq(trash.ownerOf(i), user);
+    //         originalTokenURIs[i - 1] = trash.tokenURI(i);
+    //         assertEq(originalTokenURIs[i - 1], originalOwnerTokenURIs[i - 1]);
+    //     }
+
+    //     vm.startPrank(user);
+    //     trash.setApprovalForAll(address(dumpsterBin), true);
+
+    //     uint256[] memory ids = new uint256[](numOfNfts);
+    //     for (uint256 i = 0; i < numOfNfts; i++) {
+    //         ids[i] = i + 1;
+    //     }
+
+    //     dumpsterBin.mintBatch(ids);
+
+    //     for (uint256 i = 0; i < numOfNfts; i++) {
+    //         assertEq(trash.ownerOf(i + 1), address(dumpsterBin));
+    //         assertEq(dumpsterDivers.ownerOf(i), user);
+    //     }
+
+    //     uint256[] memory ids2 = new uint256[](numOfNfts);
+    //     for (uint256 i = 0; i < numOfNfts; i++) {
+    //         ids2[i] = i;
+    //     }
+
+    //     dumpsterBin.burnBatch(ids2);
+
+    //     vm.stopPrank();
+
+    //     for (uint256 i = 1; i <= numOfNfts; i++) {
+    //         assertEq(trash.ownerOf(i), user);
+    //         vm.expectRevert();
+    //         dumpsterDivers.ownerOf(i - 1);
+    //     }
+    // }
+
+    // function testRevert__CannotMintIfNotOwnerOfBin(address addr) public {
+    //     vm.assume(addr != address(dumpsterBin));
+    //     vm.prank(user);
+    //     vm.expectRevert();
+    //     dumpsterDivers.mint(user, 0);
+    // }
 }
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -19,15 +19,15 @@ export const menuLinks: HeaderMenuLink[] = [
     label: "Home",
     href: "/",
   },
-  {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
-  },
-  {
-    label: "Swap",
-    href: "/swap",
-  },
+  // {
+  //   label: "Debug Contracts",
+  //   href: "/debug",
+  //   icon: <BugAntIcon className="h-4 w-4" />,
+  // },
+  // {
+  //   label: "Swap",
+  //   href: "/swap",
+  // },
 ];
 
 export const HeaderMenuLinks = () => {
