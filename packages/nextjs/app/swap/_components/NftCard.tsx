fcb5934ab9187e3e49a34ea36325e3c59b4c4474
Jesse Paterson <jesse.r.pat@gmail.com>
2024-03-02 15:04:13 -0700

    Cleaned up bag

--- packages/nextjs/app/swap/_components/NftCard.tsx
+++ packages/nextjs/app/swap/_components/NftCard.tsx
@@ -7,37 +7,23 @@ interface NftCardProps {
 }
 
 export const NftCard = (props: NftCardProps) => {
-  let attributesOutput;
-  if (props?.nft?.attributes?.length > 0) {
-    attributesOutput = props?.nft?.attributes?.map((attribute, index) => (
-      <p key={index}>
-        {attribute["trait_type"]}: {attribute["value"]}
-      </p>
-    ));
-  }
+  // let attributesOutput;
+  // if (props?.nft?.attributes?.length > 0) {
+  //   attributesOutput = props?.nft?.attributes?.map((attribute, index) => (
+  //     <p key={index}>
+  //       {attribute["trait_type"]}: {attribute["value"]}
+  //     </p>
+  //   ));
+  // }
 
   return (
-    <div className="flex flex-col items-center bg-black m-1 p-1">
-      {props.buttonText ? (
-        <button
-          onClick={async () => {
-            if (props.onClaimed) await props.onClaimed();
-          }}
-          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-        >
-          {props.buttonText}
-        </button>
-      ) : (
-        <></>
-      )}
-      <p>{props.nft.name}</p>
-      <p className="-m-2">Token ID: {props.nft.id.toString()}</p>
-      {attributesOutput}
+    <div className="flex flex-col items-center bg-slate m-1 p-1">
       {props?.nft?.image ? (
         <img src={props?.nft?.image.replace("ipfs://", "https://ipfs.io/ipfs/")} width={128} height={128} />
       ) : (
         <></>
       )}
+      <p>{props.nft.name}</p>
     </div>
   );
 };
