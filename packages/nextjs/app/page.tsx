5eb649a31e6b836112a5afca9c6526b8072c8db2
Jacob Homanics <homanicsjake@gmail.com>
2024-03-02 18:16:50 -0700

    hid frontend

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,12 +1,8 @@
-import SwapComp from "./swap/_components/SwapComp";
+// import SwapComp from "./swap/_components/SwapComp";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <SwapComp />
-    </>
-  );
+  return <>{/* <SwapComp /> */}</>;
 };
 
 export default Home;
